59e1a9a4a02a6cca889d01fddbf5c7f50a1c0af8
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-10-13 11:13:22 +0700

    implement export and cache playlist

---
+++ src/api/exports/handler.js
@@ -0,0 +1,29 @@
+class ExportsHandler {
+  constructor(playlistsService, producerService) {
+    this.playlistsService = playlistsService;
+    this.producerService = producerService;
+
+    this.postExportPlaylistHandler = this.postExportPlaylistHandler.bind(this);
+  }
+
+  async postExportPlaylistHandler(request, h) {
+    const { id: userId } = request.auth.credentials;
+    const { playlistId } = request.params;
+    const { targetEmail } = request.payload;
+    const result = await this.playlistsService
+      .getPlaylistSongs(playlistId, userId);
+    const message = { userId, targetEmail, result };
+
+    await this.producerService
+      .sendMessage('export:playlist', JSON.stringify(message));
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda sedang kami proses',
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
