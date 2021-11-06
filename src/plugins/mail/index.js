e8d91a22a43b8c0356015b18186047285ac19b02
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-11-06 16:10:34 +0700

    separate mail process

--- src/plugins/mail/index.js
+++
@@ -1,9 +0,0 @@
-const MailHandler = require('./handler')
-
-module.exports = {
-  name: 'mail',
-  version: '1.0.0',
-  register: async (server, { consumerService, mailService }) => {
-    const mailHandler = new MailHandler(consumerService, mailService)
-  }
-}
